74fb29ae7f564947315056e53d5ead036648221e
hollan2 <hollan2@pdx.edu>
2023-04-10 22:45:34 -0700

    logic for palindrome is created

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,28 @@
-const elem = document.querySelector('input');
+reverseString = function(str) {
+    return str.split("").reverse().join("");
+};
 
-elem.addEventListener('input', handleInput);
+isPalindrome = function() {
+    const input = document.getElementById('input');
+    const result = document.getElementById('result');
+
+    if (input.value != null && input.value >= 0) 
+    {
+        var inputString = String(input.value);
+
+        if (inputString == '')
+        {
+            result.innerHTML = "Please input a positive number";
+            return;
+        }
+
+        var inputReverse = reverseString(inputString);
+
+        if (inputString == inputReverse) 
+            result.innerHTML = "Yes! " + inputString + " is a palindrome! :D"
+        else 
+            result.innerHTML = "No! " + inputString + " is not a palindrome! :("
+    }
+    else 
+        result.innerHTML = "Please input a positive number";
+}
