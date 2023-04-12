48f87eb2b9e43471fe193da96ed51344f6be50c7
hollan2 <hollan2@pdx.edu>
2023-04-12 00:32:54 -0700

    style palindrome page

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -13,16 +13,27 @@ isPalindrome = function() {
         if (inputString == '')
         {
             result.innerHTML = "Please input a positive number";
+            result.classList.remove("text-danger");
+            result.classList.remove("text-success");
             return;
         }
 
         var inputReverse = reverseString(inputString);
 
-        if (inputString == inputReverse) 
+        if (inputString == inputReverse) {
             result.innerHTML = "Yes! " + inputString + " is a palindrome! :D"
-        else 
+            result.classList.remove("text-danger");
+            result.classList.add("text-success");
+        }
+        else {
             result.innerHTML = "No! " + inputString + " is not a palindrome! :("
+            result.classList.remove("text-success");
+            result.classList.add("text-danger");
+        }
     }
-    else 
+    else {
         result.innerHTML = "Please input a positive number";
+        result.classList.remove("text-danger");
+        result.classList.remove("text-success");
+    }
 }
