d42bd9c9ee24516b55764a2c7f7121053577a18f
hollan2 <hollan2@pdx.edu>
2023-04-26 00:16:39 -0700

    Exercise 4 solution

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,1 +1,27 @@
 // Add your code here
+
+const colors = ['#FF0000', '#FFA500', '#FFFF00', '#00FF00', '#0000FF', '#A020F0', '#FFC0CB']
+
+async function startColorChange() 
+{
+   let interval = document.getElementById("interval");
+
+   document.getElementById("start").hidden = true;
+   document.getElementById("stop").hidden = false;
+
+   while(document.getElementById("stop").hidden == false)
+   {
+        document.body.style.backgroundColor = colors[Math.floor(Math.random() * 8)];
+        await sleep(interval.value);
+   }
+}
+
+function stopColorChange() 
+{
+    document.getElementById("stop").hidden = true;
+    document.getElementById("start").hidden = false;
+}
+
+function sleep(s) {
+    return new Promise(resolve => setTimeout(resolve, s*1000));
+  }
